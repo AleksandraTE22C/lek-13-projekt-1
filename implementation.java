7c4be76f99e2d7af3b34474668365abc5af7bbaf
AleksandraTE22C <aleksandra.kajak-tonkiel@elev.ga.ntig.se>
2024-02-12 09:30:37 +0100

    Jobbat på monster attacker och poäng systemet

--- implementation.java
+++ implementation.java
@@ -2,7 +2,7 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class implementation{
-    public static void main (String[] args){
+    public static void main (String[] args) throws InterruptedException{
         Random roll = new Random();
         Scanner enter = new Scanner(System.in);
         int pHealth = 100;
@@ -13,28 +13,33 @@ public class implementation{
         int round = 0;
         System.out.print("Enter player name: ");
         String name = enter.next();
-        int score = 30;
-        System.out.println("Initial score: "+score);
+        int score = 40;
+        System.out.println("Initial points: "+score);
 
-        while(pHealth > 0 && mHealth1 > 0 && mHealth2 > 0 && mHealth3 > 0 && mHealth4 > 0){
-            if(score > 0){
+        while(pHealth > 0 ){
+            while(score>=15){
             while(mHealth1 > 0){
             round++;
+            Thread.sleep(800);
             System.out.println("Round "+round);
             System.out.println("Your turn");
             System.out.println("How will you attack?");
-            System.out.println("1. Throw rock  2. Cast Freeze  3. Cast Fireball");
+            System.out.println("Monster's HP: " + mHealth1);
+            System.out.println("Points: "+score);
+            Thread.sleep(500);
+            System.out.println("1. Throw rock (-15 points)");
+            System.out.println("2. Cast Freeze(-25 points)");
+            System.out.println("3. Cast Fireball (-50 points)");
             int atk = enter.nextInt();
             int atk1 = roll.nextInt(15);
             int atk2 = roll.nextInt(25);
             int atk3 = roll.nextInt(50);
-            int mAtk1= roll.nextInt(10);
 
             if(atk == 1){
-                score -= 15;
-                        if(atk > 0){
-                        System.out.println("You did "+atk1+" damage!");
-                        mHealth1 -= atk1;
+                    score -= 15;
+                    if(atk > 0){
+                    System.out.println("You did "+atk1+" damage!");
+                    mHealth1 -= atk1;
                     }
                     else{
                         System.out.println("You missed!");
@@ -42,7 +47,7 @@ public class implementation{
                 }
                 else if(atk == 2){
                     score -= 25;
-                        if(atk2 > 0){
+                    if(atk2 > 0){
                         System.out.println("You did "+atk2+" damage!");
                         mHealth1 -= atk2;
                     }
@@ -56,21 +61,120 @@ public class implementation{
                             System.out.println("You did "+atk2+" damage!");
                             mHealth1 -= atk3;
                         }
+                        else{
+                            System.out.println("You missed!");
+                        }
                 }
-             System.out.println("Enemys turn!");
+                
+            Thread.sleep(800);
+            System.out.println("Enemys turn");
+            int mAtk1= roll.nextInt(30);
+            if(mAtk1 > 0){
+                System.out.println("The monster did "+mAtk1+" damage!");
+                pHealth -= mAtk1;
             }
-           
+            else{
+                System.out.println("The monster missed!");
+             }
+            if (mHealth1 <= 0){
+                System.out.println("Monster defeated!");
+                score += 40;
+                round = 0;
+                Thread.sleep(800);
+                break;
+            }
+            if(pHealth <= 0){
+                System.out.println("GAME OVER");
+                break;
+            }
+            if(score < 15 && mHealth1 > 0){
+                System.out.println("Not enough points!");
+                System.out.println("GAME OVER");
+                break;
+            }
         }
-        if(pHealth <= 0){
-            System.out.println("GAME OVER");
-            break;
+
+            while(mHealth2 > 0 && mHealth1 <= 0){
+                System.out.println("A new monster appeared!");
+                round++;
+                Thread.sleep(800);
+                System.out.println("Round "+round);
+                System.out.println("Your turn");
+                System.out.println("How will you attack?");
+                System.out.println("Monster's HP: " + mHealth2);
+                System.out.println("Points: "+score);
+                Thread.sleep(500);
+                System.out.println("1. Throw rock (-15 points)");
+                System.out.println("2. Cast Freeze(-25 points)");
+                System.out.println("3. Cast Fireball (-50 points)");
+                int atk = enter.nextInt();
+                int atk1 = roll.nextInt(15);
+                int atk2 = roll.nextInt(25);
+                int atk3 = roll.nextInt(50);
+    
+                if(atk == 1){
+                        score -= 15;
+                        if(atk > 0){
+                        System.out.println("You did "+atk1+" damage!");
+                        mHealth1 -= atk1;
+                        }
+                        else{
+                            System.out.println("You missed!");
+                        }
+                    }
+                    else if(atk == 2){
+                        score -= 25;
+                        if(atk2 > 0){
+                            System.out.println("You did "+atk2+" damage!");
+                            mHealth1 -= atk2;
+                        }
+                        else{
+                            System.out.println("You missed!");
+                        }
+                    }
+                    else if(atk == 3){
+                        score -= 50;
+                            if(atk3 > 0){
+                                System.out.println("You did "+atk2+" damage!");
+                                mHealth1 -= atk3;
+                            }
+                            else{
+                                System.out.println("You missed!");
+                            }
+                    }
+                Thread.sleep(800);
+                if(mHealth2>0){
+                System.out.println("Enemys turn");
+                int mAtk2= roll.nextInt(40);
+                if(mAtk2 > 0){
+                    System.out.println("The monster did "+mAtk2+" damage!");
+                    pHealth -= mAtk2;
+                }
+                else{
+                    System.out.println("The monster missed!");
+                 }
+                }
+                if (mHealth2 <= 0){
+                    System.out.println("Monster defeated!");
+                    score += 50;
+                    break;
+                }
+                if(pHealth <= 0){
+                    System.out.println("GAME OVER");
+                    break;
+                }
+                if(score < 15 && mHealth2 > 0){
+                    System.out.println("Not enough points!");
+                    System.out.println("GAME OVER");
+                    break;
+                }
+
         }
         if(mHealth4 <= 0){
             System.out.println("You Won!");
             break;
-        } 
-    }
-        
+        }
     }
 }
-
+    }
+}
