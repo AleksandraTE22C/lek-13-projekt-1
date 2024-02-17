167a2a92ee8448430850e4875032965fdba7cdeb
AleksandraTE22C <aleksandra.kajak-tonkiel@elev.ga.ntig.se>
2024-02-17 14:49:03 +0100

    La till en leaderboard med en hashmap och filereader

--- implementation.java
+++ implementation.java
@@ -1,6 +1,14 @@
 import java.util.Random;
 import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 class dmgResult {
@@ -14,25 +22,30 @@ class dmgResult {
 }
 
 public class implementation{
+    private static final String board = "leaderboard.txt";
     static Random roll = new Random();
     static Scanner enter = new Scanner(System.in);
+    static boolean gameWon = false;
     static int pHealth = 100;
     static int round = 0;
     static Map<String, Integer> leaderboard = new HashMap<>();
 
     public static void main (String[] args) throws InterruptedException{
-        int score = 50;
+        loadLeaderboard();
         System.out.print("Enter player name: ");
         String name = enter.next();
+        int score = 50;
 
-        while(pHealth>0){
+        while(pHealth>0 && !gameWon){
         score = battle(25, 55, score);
         score = battle(35, 70, score);
         score = battle(50, 100, score);
+
         if(pHealth > 0 && score > 14){
             System.out.println("BOSS INCOMING");
         }
         score = bossBattle(100, 100, score);
+
         if (score < 15 && pHealth > 0) {
             System.out.println("You ran out of points!");
             break;
@@ -41,6 +54,59 @@ public class implementation{
             System.out.println("GAME OVER");
             break;
         }
+    }
+    System.out.println();
+    Thread.sleep(800);
+    if (gameWon) {
+        updateLeaderboard(name, score);
+        displayLeaderboard();
+        saveLeaderboard();
+    }
+}
+
+    static void updateLeaderboard(String name, int score) {
+        if (leaderboard.containsKey(name)) {
+            int currentScore = leaderboard.get(name);
+            leaderboard.put(name, currentScore + score);
+        } else {
+            leaderboard.put(name, score);
+        }
+    }
+
+    static void displayLeaderboard() {
+        System.out.println("---------------------------------------------");
+        System.out.println("\t\tLEADERBOARD");
+        System.out.println("---------------------------------------------");
+        System.out.println("Player Name\t\t\tScore");
+
+        List<Map.Entry<String, Integer>> highScores = leaderboard.entrySet().stream()
+        .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+        .collect(Collectors.toList());
+
+        for (Map.Entry<String, Integer> entry : leaderboard.entrySet()) {
+            System.out.println(entry.getKey() + "\t\t\t\t" + entry.getValue());
+        }
+    }
+
+        static void saveLeaderboard() {
+        try (PrintWriter writer = new PrintWriter(new FileWriter(board))) {
+            for (Map.Entry<String, Integer> entry : leaderboard.entrySet()) {
+                writer.println(entry.getKey() + "," + entry.getValue());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    static void loadLeaderboard() {
+        try (BufferedReader reader = new BufferedReader(new FileReader(board))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(",");
+                leaderboard.put(parts[0], Integer.parseInt(parts[1]));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
@@ -99,7 +165,8 @@ public class implementation{
                     System.out.println("The monster did " + mAtk + " damage!");
                     pHealth -= mAtk;
                 }
-            } return score;
+            } 
+            return score;
         }
 
     static int bossBattle(int bossHealth, int scorePlus, int score) throws InterruptedException {
@@ -144,9 +211,9 @@ public class implementation{
 
                 if (bossHealth <= 0) {
                     System.out.println("BOSS defeated!");
-                    System.out.println("YOU WIN");
+                    System.out.println("YOU WON");
                     score += scorePlus;
-                    round = 0;
+                    gameWon = true;
                     break;
                 }
 
@@ -168,11 +235,11 @@ public class implementation{
             case 1:
                 if (score > 14) {
                     updScore -= 15;
-                    dmg = roll.nextInt(25);
-                    if(dmg > 0 && dmg < 25){
-                        dmg += 5;
-                        if(dmg > 25){
-                            dmg = 25;
+                    dmg = roll.nextInt(15);
+                    if(dmg > 0 && dmg < 15){
+                        dmg += 10;
+                        if(dmg > 15){
+                            dmg = 15;
                         }
                     }
                 } else {
@@ -183,11 +250,11 @@ public class implementation{
             case 2:
                 if (score > 24) {
                     updScore -= 25;
-                    dmg = roll.nextInt(35);
-                    if(dmg > 0 && dmg < 35){
-                        dmg += 10;
-                        if(dmg > 35){
-                            dmg = 35;
+                    dmg = roll.nextInt(25);
+                    if(dmg > 0 && dmg < 25){
+                        dmg += 15;
+                        if(dmg > 25){
+                            dmg = 25;
                         }
                     }
                 } else {
@@ -196,13 +263,13 @@ public class implementation{
                 break;
     
             case 3:
-                if (score > 35) {
+                if (score > 34) {
                     updScore -= 35;
-                    dmg = roll.nextInt(45);
-                    if(dmg > 0 && dmg < 45){
-                        dmg += 15;
-                        if(dmg > 45){
-                            dmg = 45;
+                    dmg = roll.nextInt(35);
+                    if(dmg > 0 && dmg < 35){
+                        dmg += 20;
+                        if(dmg > 35){
+                            dmg = 35;
                         }
                     }
                 } else {
